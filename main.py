27a1c3be8e083760212762a8d59bd8f561cb31ca
Mykola Solodukha <mykola.soloduha@gmail.com>
2022-09-01 23:39:41 +0300

    :loud_sound: Use `loguru` for logging
    Signed-off-by: Mykola Solodukha <mykola.soloduha@gmail.com>

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 import aiogram
 
 from settings import settings
+from utils.logging import logger
 
 bot = aiogram.Bot(settings.TELEGRAM_BOT_TOKEN)
 dp = aiogram.Dispatcher(bot)
@@ -10,6 +11,7 @@ dp = aiogram.Dispatcher(bot)
 @dp.message_handler(aiogram.filters.CommandStart())
 async def start(message: aiogram.types.Message):
     """`/start` command handler."""
+    logger.info(f"Received /start command: {message.text=} from {message.from_user.to_python()=}")
     me = await bot.get_me()
     await message.answer(
         f"Hi! I'm the {me.full_name} bot. Send me a message and I'll reply to you."
