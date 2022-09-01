c8bd82283f7464d488da05c8c7119352e5782b09
Mykola Solodukha <mykola.soloduha@gmail.com>
2022-09-01 22:45:48 +0300

    :wrench: Create a `settings` module using `pydantic.BaseSettings`
    Signed-off-by: Mykola Solodukha <mykola.soloduha@gmail.com>

---
+++ settings.py
@@ -0,0 +1,16 @@
+"""The module for the settings of the application."""
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    """Settings for the Telegram bot."""
+
+    TELEGRAM_BOT_TOKEN: str
+
+    class Config:
+        """Configuration for the settings."""
+
+        env_file = ".env"
+
+
+settings = Settings()
