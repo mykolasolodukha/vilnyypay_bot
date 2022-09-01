24b40b05c6bc78f61bd0b425af3bce15d25e2b96
Mykola Solodukha <mykola.soloduha@gmail.com>
2022-09-02 00:17:15 +0300

    :sparkles: Initiate and shutdown a DB connection using respective hooks
    Signed-off-by: Mykola Solodukha <mykola.soloduha@gmail.com>

--- main.py
+++ main.py
@@ -2,12 +2,14 @@
 import aiogram
 
 from settings import settings
+from utils import tortoise_orm
 from utils.logging import logger
 
 bot = aiogram.Bot(settings.TELEGRAM_BOT_TOKEN)
 dp = aiogram.Dispatcher(bot)
 
 
+# region Handlers
 @dp.message_handler(aiogram.filters.CommandStart())
 async def start(message: aiogram.types.Message):
     """`/start` command handler."""
@@ -18,5 +20,31 @@ async def start(message: aiogram.types.Message):
     )
 
 
+# endregion
+
+
+# region Startup and shutdown callbacks
+async def on_startup(*_, **__):
+    """Startup the bot."""
+    logger.info(f"Starting up the https://t.me/{(await bot.get_me()).username} bot...")
+
+    logger.debug("Initializing the database connection...")
+    await tortoise_orm.init()
+
+    logger.info("Startup complete.")
+
+
+async def on_shutdown(*_, **__):
+    """Shutdown the bot."""
+    logger.info("Shutting down...")
+
+    logger.debug("Closing the database connection...")
+    await tortoise_orm.shutdown()
+
+    logger.info("Shutdown complete.")
+
+
+# endregion
+
 if __name__ == "__main__":
-    aiogram.executor.start_polling(dp)
+    aiogram.executor.start_polling(dp, on_startup=on_startup, on_shutdown=on_shutdown)
