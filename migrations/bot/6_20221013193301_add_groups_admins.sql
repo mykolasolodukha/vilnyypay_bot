ea821e3d414cf5bbe76151c74585e8ade191dedf
Mykola Solodukha <mykola.soloduha@gmail.com>
2022-11-04 11:29:42 +0200

    :card_file_box: Set `Group` creators as default `Group` admins

--- migrations/bot/6_20221013193301_add_groups_admins.sql
+++ migrations/bot/6_20221013193301_add_groups_admins.sql
@@ -4,5 +4,11 @@ CREATE TABLE "group__admin"
     "group_id" INT    NOT NULL REFERENCES "group" ("id") ON DELETE CASCADE,
     "user_id"  BIGINT NOT NULL REFERENCES "user" ("id") ON DELETE CASCADE
 );
+
+-- Set default group admins
+INSERT INTO "group__admin" ("group_id", "user_id")
+SELECT "id", "created_by_user_id"
+FROM "group";
+
 -- downgrade --
 DROP TABLE IF EXISTS "group__admin";
